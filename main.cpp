127223618625b75b543fc1fea1b4c595683307de
Kyle Byassee <kylhbyas@ut.utm.edu>
2022-08-25 12:49:56 -0500

    Extended main to do a sum

--- main.cpp
+++ main.cpp
@@ -12,8 +12,35 @@
 
 using namespace std;
 
+
+/**
+ * Sums ints from 1 to n
+ *
+ * @param int n User prompted int
+ * @return int Sum from 1 to n
+ * @post Printed sum
+ * 
+ */
+int sum(int n);
+
 int main() {
   cout << "Hello World!" << endl;
+
+  int n = 0;
+  cout << "Enter a number: ";
+  cin >> n;
+
+  cout << "Sum: " << sum(n) << endl;
   
   return 0;
+}
+
+int sum(int n) {
+  int sum = n;
+  
+  for(int i = (n - 1); i > 0; i--) {
+    sum += i;
+  }
+  
+  return sum;
 }
