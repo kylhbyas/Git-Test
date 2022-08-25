782eada182d26dc2edefddd771404541663b13e9
Kyle Byassee <kylhbyas@ut.utm.edu>
2022-08-25 12:03:31 -0500

    First file of the project

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/**
+ * @file main.cpp
+ * @author Kyle Byassee
+ * @date 2022-08-25
+ * @brief Hello world program
+ * 
+ * A program for testing
+ */
+
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  cout << "Hello World!" << endl;
+  
+  return 0;
+}
